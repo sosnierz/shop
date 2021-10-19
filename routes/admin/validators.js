20c717e00375fff0ae38e4d50cc09f7da2bda6ce
sosnierz <37373004+sosnierz@users.noreply.github.com>
2021-10-19 20:28:03 +0200

    Create validators.js

---
+++ routes/admin/validators.js
@@ -0,0 +1,68 @@
+const { check } = require('express-validator');
+const usersRepo = require('../../repositories/users');
+
+module.exports = {
+  requireTitle: check('title')
+    .trim()
+    .isLength({ min: 5, max: 40 })
+    .withMessage('Od 5 do 40 znaków'),
+  requirePrice: check('price')
+    .trim()
+    .toFloat()
+    .isFloat({ min: 1 })
+    .withMessage('Wartość musi być większa od 1'),
+  requireText:check('opis')
+    .isLength({ min: 5, max: 1500 })
+    .withMessage('Od 5 do 1500 znaków'),
+  requireEmail: check('email')
+    .trim()
+    .normalizeEmail()
+    .isEmail()
+    .withMessage('Email musi być poprawny')
+    .custom(async email => {
+      const existingUser = await usersRepo.getOneBy({ email });
+      if (existingUser) {
+        throw new Error('Email został już użyty');
+      }
+    }),
+  requirePassword: check('password')
+    .trim()
+    .isLength({ min: 4, max: 20 })
+    .withMessage('Od 4 do 20 znaków'),
+  requirePasswordConfirmation: check('passwordConfirmation')
+    .trim()
+    .isLength({ min: 4, max: 20 })
+    .withMessage('Od 4 do 20 znaków')
+    .custom(async (passwordConfirmation, { req }) => {
+      if (passwordConfirmation !== req.body.password) {
+        throw new Error('Hasła muszą pasować');
+      }
+    }),
+  requireEmailExists: check('email')
+    .trim()
+    .normalizeEmail()
+    .isEmail()
+    .withMessage('Podaj poprawny email')
+    .custom(async email => {
+      const user = await usersRepo.getOneBy({ email });
+      if (!user) {
+        throw new Error('Email nieznaleziony!');
+      }
+    }),
+  requireValidPasswordForUser: check('password')
+    .trim()
+    .custom(async (password, { req }) => {
+      const user = await usersRepo.getOneBy({ email: req.body.email });
+      if (!user) {
+        throw new Error('Niepoprawne hasło');
+      }
+
+      const validPassword = await usersRepo.comparePasswords(
+        user.password,
+        password
+      );
+      if (!validPassword) {
+        throw new Error('Niepoprawne hasło');
+      }
+    })
+};
