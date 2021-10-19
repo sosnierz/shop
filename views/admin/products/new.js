d82a7c3635cbb2bab0bae80833070980c8711f60
sosnierz <37373004+sosnierz@users.noreply.github.com>
2021-10-19 20:28:32 +0200

    Create new.js

---
+++ views/admin/products/new.js
@@ -0,0 +1,42 @@
+const layout = require('../layout');
+const { getError } = require('../../helpers');
+
+module.exports = ({ errors }) => {
+  return layout({
+    content: `
+      <div class="columns is-centered">
+        <div class="column is-half">
+          <h1 class="subtitle">Dodaj produkt</h1>
+
+          <form method="POST" enctype="multipart/form-data">
+            <div class="field">
+              <label class="label">Nazwa</label>
+              <input class="input" placeholder="Nazwa" name="title">
+              <p class="help is-danger">${getError(errors, 'title')}</p>
+            </div>
+            
+            <div class="field">
+              <label class="label">Cena</label>
+              <input class="input" placeholder="Cena" name="price">
+              <p class="help is-danger">${getError(errors, 'price')}</p>
+            </div>
+            
+            <div class="field">
+              <label class="label">Zdjęcie</label>            
+              <input type="file" name="image" />
+            </div>
+
+            <div class="field">
+            <label class="label">Informacje o produkcie</label>
+            <textarea class="input" name="opis" rows="200" cols="150"></textarea>
+            <p class="help is-danger">${getError(errors, 'opis')}</p>
+          </div>
+
+            <br />
+            <button class="button is-primary">Utwórz</button>
+          </form>
+        </div>
+      </div>
+    `
+  });
+};
