e0a120be5f8c99974d0f6b6c1fbf7b02d87ed336
sosnierz <37373004+sosnierz@users.noreply.github.com>
2021-10-25 13:52:43 +0200

    step-2

--- views/page/index.js
+++ views/page/index.js
@@ -24,27 +24,7 @@ module.exports =  () => {
             </div>
           </nav> 
         </section>
-        <section class="shop">
-        <div class = "products">
-        <div  id="firstProduct" class="product">
-        <a href="#"> <h1> Polecane </h1></a>
-        </div>
-        <div class="products">
-        <div class="column is-one-quarter">
-        <div class="card product-card">
-          <figure>
-            <img src=""/>
-          </figure>
-          <div class="card-content">
-            <h3 class="subtitle"></h3>
-            <h5></h5>
-           </div>
-         
-        </div>
-      </div> 
-        </div> 
-        </div>
-        </section>
+       
         <section class="conditions">
         <div class="condition">
         <img class="imgCondition" src="/images/freeDelivery.png" alt="">
@@ -67,6 +47,30 @@ module.exports =  () => {
         <p>Wykonaj przelew ze swojego konta, prześlij nam potwierdzenie lub zapłać przyt odbiorze</p>
         </div> 
         </section>
+        
+        <section class="gallery">
+          <div class="images">
+            <div class="image">
+            <a href=""></a>
+            <h2>Wszystkie artykuły</h2></div>
+            <div class="image">
+            <a href=""><img src="/images/artykul.jpg" alt=""></a>
+            <h2>Lampy nowoczesne</h2></div>
+            <div class="image">
+            <a href=""><img src="/images/artykul.jpg" alt=""></a>
+            <h2>Lampy klasyczne</h2></div>
+            <div class="image">
+            <a href=""><img src="/images/artykul.jpg" alt=""></a>
+            <h2>Lampy dziecięce</h2></div>
+            <div class="image">
+            <a href=""><img src="/images/artykul.jpg" alt=""></a>
+            <h2>Lampy stołowe</h2></div>
+            <div class="image">
+            <a href=""><img src="/images/artykul.jpg" alt=""></a>
+            <h2>Lampy podłogowe</h2></div>
+          </div>
+        </section>
+       
         <script src="/js/home.js"></script>
       `
     });
